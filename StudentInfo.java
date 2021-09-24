294b2d4306c412ec909a558608093cf01db294ca
Sophie <82679945+acsharrison@users.noreply.github.com>
2021-09-24 17:35:22 -0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,18 @@
+/**
+*A program to print StudentInfo. *
+* Activity 1
+* Sophie Harrison – COMP 1213 
+* January 19 2021
+*/
+public class StudentInfo {
+/**
+* StudentInfo program. * 
+*@param args Command line arguments – not used.
+*/
+   public static void main(String[] args) {
+      System.out.println("Name: Sophie Harrison");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   Intro to Software Engineering - ENGR 1110");
+      System.out.println("   AP Computer Science Principles");
+   }
+}
